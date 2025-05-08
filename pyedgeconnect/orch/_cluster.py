75fa1e71cda3c302ac735286d9892d5b676e6dff
adamwilkins-hpe <adam.wilkins@hpe.com>
2025-05-08 14:41:51 -0500

    updated docstring for 'get_cluster_state' function and added 'get_cluster_alarm_count' function

--- pyedgeconnect/orch/_cluster.py
+++ pyedgeconnect/orch/_cluster.py
@@ -5,19 +5,45 @@
 
 
 def get_cluster_state(self, cluster_name: str, cached: bool) -> dict:
-    """
-    Get the state of a cluster.
+    """Get the state of a cluster.
 
-    Args:
-        request: The HTTP request object
-        **kwargs: Additional keyword arguments
+    .. list-table::
+        :header-rows: 1
 
-    Query Parameters:
-        clusterName: Site / Cluster Name
-        cached: (Required) Get data from cache (true) or from appliance (false)
+        * - Swagger Section
+          - Method
+          - Endpoint
+        * - cluster
+          - GET
+          - /cluster
 
-    Returns:
-        JSON response with cluster state information
+    :param cluster_name: Site / Cluster Name
+    :type cluster_name: str
+    :param cached: Boolean indicating whether to get data from cache (true)
+        or from appliance (false)
+    :type cached: bool
+    :return: Returns dictionary of cluster state information \n
+        * keyword **<siteName>** (`dict`): Dictionary with site information \n
+            * keyword **siteName** (`str`): Name of the site/cluster
+            * keyword **inSync** (`bool`): Whether the cluster is in sync
+            * keyword **nonce** (`float`): Nonce value for the cluster
+            * keyword **siteApplianceInterfaceIps** (`dict`): Dictionary of appliance
+                interface IPs keyed by network primary key (nePk) \n
+                * keyword **<nePk>** (`dict`): Dictionary with appliance information \n
+                    * keyword **applianceName** (`str`): Name of the appliance
+                    * keyword **interfaceLabel** (`str`): Label of the interface
+                    * keyword **interfaceName** (`str`): Name of the interface
+                    * keyword **state** (`dict`): State information for the interface \n
+                        * keyword **peerIPStatusList** (`list[dict]`): List of peer IP statuses \n
+                            * keyword **peerIP** (`str`): IP address of the peer
+                            * keyword **peerIPState** (`str`): State of the peer IP
+                        * keyword **cluster** (`bool`): Whether clustering is enabled
+                        * keyword **flow_redir** (`bool`): Whether flow redirection is enabled
+                        * keyword **wait_time** (`float`): Wait time value
+                        * keyword **interface** (`str`): Interface name
+                        * keyword **user_sync** (`bool`): Whether user sync is enabled
+                    * keyword **ip** (`str`): IP address of the interface
+    :rtype: dict
     """
 
     if self.orch_version < 9.5:
@@ -26,5 +52,32 @@ def get_cluster_state(self, cluster_name: str, cached: bool) -> dict:
         )
     else:
         path = f"/cluster?clusterName={cluster_name}&cached={cached}"
+
+    return self._get(path)
+
+
+def get_cluster_alarm_count(self) -> dict:
+    """Get the alarm count for the cluster.
+
+    .. list-table::
+        :header-rows: 1
+
+        * - Swagger Section
+          - Method
+          - Endpoint
+        * - cluster
+          - GET
+          - /cluster/alarmCount
+
+    :return: Returns dictionary containing alarm count information for the cluster
+    :rtype: dict
+    """
+
+    if self.orch_version < 9.5:
+        raise ValueError(
+            "Cluster alarm count is only supported on Orchestrator 9.5 and above"
+        )
+    else:
+        path = "/cluster/alarmCount"
 
     return self._get(path)
