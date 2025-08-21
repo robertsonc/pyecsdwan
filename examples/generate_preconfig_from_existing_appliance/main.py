1cfa8822ff8c6f8850744c1c47788af92bd65f29
adamwilkins-hpe <adam.wilkins@hpe.com>
2025-08-21 13:33:56 -0500

    commit

--- examples/generate_preconfig_from_existing_appliance/main.py
+++ examples/generate_preconfig_from_existing_appliance/main.py
@@ -1,10 +1,12 @@
 from DeploymentInfo import DeploymentInfo
 from ApplianceInfo import ApplianceInfo
+from PreconfigFromExisting import PreconfigFromExisting
 
 def main():
     #TODO: create class that aggregates all these classes so only one call needs to be made
-    DeploymentInfo("6.NE", upload_to_orch=False)
-    ApplianceInfo("6.NE", upload_to_orch=False)
+    DeploymentInfo("2.NE", upload_to_orch=False)
+    # ApplianceInfo("0.NE", upload_to_orch=False)
+    # PreconfigFromExisting("6.NE", upload_to_orch=False)
 
 if __name__ == '__main__':
     main()
